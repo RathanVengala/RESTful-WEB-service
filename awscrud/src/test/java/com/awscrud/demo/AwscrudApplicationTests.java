cfc7549bcc9418e7b51ad567bfbafad3b06ed971
Rathan <rathanvengala@gmail.com>
2021-12-09 12:56:10 +0530

    Add files via upload

---
+++ awscrud/src/test/java/com/awscrud/demo/AwscrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.awscrud.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwscrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
